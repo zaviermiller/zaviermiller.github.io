2c42edf15b3158e28adcbe5741ba89c72f9af333
Zavier <zavier@resaleai.com>
2021-03-27 14:45:46 -0400

    98307ab

---
+++ _nuxt/static/1616870742/boids/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/boids", {data:[{}],fetch:[],mutations:[]});
---
+++ _nuxt/static/1616870742/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fjournal","\u002Fabout","\u002Fboids","\u002Fprojects"]})
